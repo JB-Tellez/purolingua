0cd3402bfbe963bd2cd3ee272ce993e8e5f5dc6a
jb.telez <jb.tellez@galvanize.com>
2026-01-13 21:13:38 -0800

    moves indietro button to nav bar

--- src/js/core/views.js
+++ src/js/core/views.js
@@ -1,21 +1,24 @@
 // View Management - Functions for managing view state and navigation
 
 // DOM Elements (will be initialized on DOM load)
-let deckSelectionView, flashcardView;
+let deckSelectionView, flashcardView, backButton;
 
 function initializeViewElements() {
     deckSelectionView = document.getElementById('deck-selection');
     flashcardView = document.getElementById('flashcard-view');
+    backButton = document.getElementById('back-button');
 }
 
 function switchToFlashcardView() {
     deckSelectionView.classList.add('hidden');
     flashcardView.classList.remove('hidden');
+    backButton.classList.remove('hidden');
 }
 
 function switchToDeckSelectionView() {
     deckSelectionView.classList.remove('hidden');
     flashcardView.classList.add('hidden');
+    backButton.classList.add('hidden');
 }
 
 function isFlashcardViewVisible() {
